b13521571bc26a500ccfd765485c201b01a40f86
Kanabh <rkanabh@gmail.com>
2026-08-17 23:29:07 +0530

    Add VerifyJWT auth middleware

---
+++ Backend/Middleware/Auth.middleware.js
@@ -0,0 +1,24 @@
+import { ApiError } from "../Utils/ApiError.js";
+import { ApiResponse } from "../Utils/ApiResponse.js";
+import { AsyncHandler } from "../Utils/AsyncHandler.js";
+import { User } from "../MongoDB/Models/UserSchema.js";
+import jwt from "jsonwebtoken";
+const VerifyJWT = AsyncHandler(async (req, res, next) => {
+  try {
+    const Token = req.cookies?.accessToken || req.header("Authorization").replace("Bearer ", "");
+    if (!Token) {
+      throw new ApiError(401, "Unauthorized user");
+    }
+    const DecodedToken = jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(DecodedToken?._id).select("-password -RefreshToken");
+    if (!user) {
+      throw new ApiError(401, "Invalid Token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid Access Token");
+  }
+});
+
+export { VerifyJWT };
